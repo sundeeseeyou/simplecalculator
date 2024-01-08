d190eb2aff4d47893cd942dcda2936323b2b3311
sundeeseeyou <sandiseeyou@gmail.com>
2024-01-08 19:00:09 +0700

    Create the data-operator for math operation

--- calc.js
+++ calc.js
@@ -1,39 +1,67 @@
-let input1 = "";
+
 let input2 = "";
 
 function clearDisplay(){
     $("#calcInput").val("");
 }
 
+
 //first value inputted
 
 $(".digit").click((evt) => {
-    let currentValue = $("input").val();
+    let currentValue = $("#calcInput").val();
     // Append the clicked button's value to the current input value
-    $("input").val(currentValue + evt.target.innerHTML);
-    input1 = parseInt($("input").val(), 10);
+    $("#calcInput").val(currentValue + evt.target.innerHTML);
+    
 });
 
 //second value inputted
 
-$(".plus").click((evt) => {
-    
+
+//plus button pressed
+$(".op").click((evt) => {
+    let input1 = parseFloat($("#calcInput").val());
+    let operator = $(evt.target).data("operator");
+
+    switch(operator) {
+        case "plus":
+            calculation(input1,2,plus);
+            break;
+        case "minus":
+            calculation(input1,2,minus)
+            break;
+            default:
+                break;
+    }
+
 })
 
 
 
 function plus(num1,num2){
-    $("input").val(num1+num2);
+   console.log (num1+num2);
 }
 
 function minus(num1,num2){
     console.log(num1-num2);
 }
 
-function calculation(num1,num2,operator){
-    operator(num1,num2);
+function calculation(num1,num2,operatorFunction){
+    operatorFunction(num1,num2);
 }
-
+    // switch (evt){
+    //     case plus:
+    //         alert("plus");
+    //         break;
+    //     case minus:
+    //         break;
+    //     case times:
+    //         break;
+    //     case divide:
+    //         break;
+    //     default:
+    
+    // }
 
 //1. Create a calculator numbers using grid
 
